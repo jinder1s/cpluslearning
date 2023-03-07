c21de6e1749fe40f9c48bca2dae471f91da2fab5
Manjinder Singh <msingh15@tufts.edu>
2023-03-07 17:01:33 -0500

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,23 +1,9 @@
 #include <cstdio>
 
-int step_function(int x) {
-  int result = 0;
-  if (x < 0) {
-    return -1;
-  } else if (x > 0) {
-    return 1;
-  }else{
-      return 0;
-  }
-}
-int absolute_value(int x){
-    if (x >= 0) return x;
-    return -1*x;
-}
-
-int get_number(int x) { return 5 * x; }
-
 int main() {
-  int x = get_number(-5);
-  printf("absolute val: %d, %d\n",x, absolute_value(x));
+
+    float a = 0.1F;
+  double an = 6.0221409e23;
+  printf("Avgado: %le, %lf, %lg\n", an, an, an);
+  return 0;
 }
