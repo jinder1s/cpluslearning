e86bc9fcbd4e09b6d99051e911b64d4abdae5db8
Manjinder Singh <msingh15@tufts.edu>
2023-03-13 16:20:48 -0400

    working vec3.h

--- raytracer/vec3.h
+++ raytracer/vec3.h
@@ -1,5 +1,5 @@
-#ifndef VEC3_H_
-#define VEC3_H_
+#ifndef VEC3_H
+#define VEC3_H
 
 #include <cmath>
 #include <iostream>
@@ -9,10 +9,7 @@ using std::sqrt;
 class vec3 {
 public:
   vec3() : e{0, 0, 0} {}
-  vec3(double e0, double e1, double e2) : e{e0, e1, e2} {
-    std::cout << "What up"<< std::endl;
-
-  }
+  vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
   double x() const { return e[0]; }
   double y() const { return e[1]; }
@@ -39,16 +36,16 @@ public:
   vec3 &operator/=(const double t) { return *this *= 1 / t; }
 
   double length() const { return sqrt(length_squared()); }
+
   double length_squared() const {
     return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
   }
 
 public:
-  double e[];
+  double e[3];
 };
 
 // Type aliases for vec3
-
 using point3 = vec3; // 3D point
 using color = vec3;  // RGB color
 
@@ -65,6 +62,7 @@ inline vec3 operator+(const vec3 &u, const vec3 &v) {
 inline vec3 operator-(const vec3 &u, const vec3 &v) {
   return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
+
 inline vec3 operator*(const vec3 &u, const vec3 &v) {
   return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
@@ -73,9 +71,9 @@ inline vec3 operator*(double t, const vec3 &v) {
   return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
-inline vec3 operator*(vec3 &v, double t) { return t * v; }
+inline vec3 operator*(const vec3 &v, double t) { return t * v; }
 
-inline vec3 operator/(vec3 &v, double t) { return (1 / t) * v; }
+inline vec3 operator/(vec3 v, double t) { return (1 / t) * v; }
 
 inline double dot(const vec3 &u, const vec3 &v) {
   return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
@@ -87,9 +85,5 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
               u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(vec3 v){
-    return v / v.length();
-}
-
-
-#endif // VEC3_H_
+inline vec3 unit_vector(vec3 v) { return v / v.length(); }
+#endif // VEC3_H
